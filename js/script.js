493d86d107e2b6f7c7feaf76d7fe15bea4cd031a
Rafal Rakoczy <rafal.p.rakoczy@gmail.com>
2020-07-01 12:47:00 +0200

    Add playerMove2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
 let playerMove = `papier`;
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
