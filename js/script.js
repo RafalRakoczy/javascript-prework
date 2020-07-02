753a811d856f61bf0ba0264bc35793838f257cfc
Rafal Rakoczy <rafal.p.rakoczy@gmail.com>
2020-07-02 23:14:55 +0200

    Add corrections after first mentor's check

--- js/script.js
+++ js/script.js
@@ -24,43 +24,22 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-}
-else if(playerInput == '2'){
+} else if(playerInput == '2'){
     playerMove = 'papier';
-  }
-else if(playerInput == '3'){
+  } else if(playerInput == '3'){
     playerMove = 'nożyce';
-  }
-else if(playerInput !== '1,2,3'){
+  } else if(playerInput !== '1,2,3'){
     playerMove = 'nieznany ruch. Wpisz 1 lub 2 lub 3';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  }
-else if( computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Ja wygrywam');
-  }
-else if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis');
-}
-else if( computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis');
-  }
-else if( computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-  }
-else if( computerMove == 'papier' && playerMove == 'kamień'){
+if(computerMove == 'papier' && playerMove == 'kamień'){
     printMessage('Ja wygrywam!');
-}
-else if( computerMove == 'nożyce' && playerMove == 'papier'){
+  }else if (computerMove == 'kamień' && playerMove == 'nożyce'){
     printMessage('Ja wygrywam!');
-  }
-else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Remis!');
-  }
-else if( computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz@!');
+  }else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ja wygrywam!');
+  }else if (computerMove ==  playerMove) {printMessage('Remis');
+}else {PrintMessage ('Ty wygrywasz');
 }
